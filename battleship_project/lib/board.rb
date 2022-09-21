e701078ce0eb43b11a1709f9b29651a9959ef673
korycfitz <korycfitz@gmail.com>
2022-09-21 10:56:51 -0400

    first 3 specs pf part 2 passed

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -2,12 +2,13 @@ class Board
     attr_reader :size
     def initialize(n)
         @grid = Array.new(n) {Array.new(n, :N)}
-        @size  = (n * n) #total size of grid
+        @size = (n * n) #total size of grid
     end
 
     def [](position) #accepts position on board (array)
         row = position[0]
         column = position[1]
+        #can also define position like this: row, column = position
         @grid[row][column] #returns element of grid at position (:Sym)
     end
 
@@ -27,5 +28,20 @@ class Board
         counter         
     end
 
+    def attack(position)
+        # row = position[0]
+        # column = position[1]
+        current_pos = self.[](position) #sets current_pos to @grid[row][column] or a position
+        current_pos=(:S) #sets current_pos= to :S
+        if pos == :S
+            self.[]=(position, :H)
+        # if self.[](position) == :S
+        # if @grid[row][column] == :S
+            # @grid[row][column] = :H    #problem with either of these ways is that we are not calling Board#[] or Board[]=
+            puts 'you sunk my battleship!'
+        end
+            
+
+    end
 
 end
