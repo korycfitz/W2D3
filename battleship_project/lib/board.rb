98e3f84112a8261be692ade263689f1f018a24ca
korycfitz <korycfitz@gmail.com>
2022-09-21 16:08:02 -0400

    finished specs in part 2 up to hidden_ships_grid: mutating the @grid instance variable. Tried using @grid.map(&:object_id) and hidden_grid = @grid.dup    hidden_grid.map(&:object_id), and I am getting different object_ids but changing my hidde_grid(dupped version of grid) still changes my @grid instance variable

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,4 +1,6 @@
 class Board
+    require 'byebug'
+
     attr_reader :size
     def initialize(n)
         @grid = Array.new(n) {Array.new(n, :N)}
@@ -12,7 +14,7 @@ class Board
         @grid[row][column] #returns element of grid at position (:Sym)
     end
 
-    def []=(position, value)
+    def []= (position, value)
         row = position[0]
         column = position[1]
         @grid[row][column] = value
@@ -27,21 +29,76 @@ class Board
         end
         counter         
     end
+                            #   # Note:
+                            #   board[pos] = :S       # this line...
+                            #   board.[]=(pos, :S)    # is same as this line. :)
+    def attack(position) #array             [  position ]
+#       # if self[position] #if board_instance[  [3, 4]   ]  
+#         #if board_instance[position] 
+#         #if self[position] = :S
+#         if self[position] = :S #same as writing  self.[]=(position, :H) ^
+#             self.[]=(position, :H)
+#             puts 'you sunk my battleship!'
+#             return true
+#         end
 
-    def attack(position)
-        # row = position[0]
-        # column = position[1]
-        current_pos = self.[](position) #sets current_pos to @grid[row][column] or a position
-        current_pos=(:S) #sets current_pos= to :S
-        if pos == :S
-            self.[]=(position, :H)
-        # if self.[](position) == :S
-        # if @grid[row][column] == :S
+#         if self[position] != :S
+#             self[position] = :X
+#             return false
+#         end
+
+
+
+#     end
+# 
+#the # code below subbed in for each line below it doesn't call on board so it doesnt pass one of the specs tried to solve above but couldn't solved below
+        row = position[0]
+        column = position[1]
+        # if @grid[row][column] == :S 
+        if self.[](position) == :S     #if value (at instance) = :S
             # @grid[row][column] = :H    #problem with either of these ways is that we are not calling Board#[] or Board[]=
+            self.[]=(position, :H)     #value= (at instance) :H
+            # self[position]= :H        #same as code above
             puts 'you sunk my battleship!'
+            return true
         end
-            
-
+        if @grid[row][column] != :S
+            # @grid[row][column] = :X
+            self.[]=(position, :X)
+            return false
+        end
     end
 
+    def place_random_ships
+        while @grid.flatten.count(:S) < @size / 4
+            #random generate row|column
+            row = rand(0...@grid.length)
+            col = rand(0...@grid[0].length)
+            @grid[row][col] = :S
+        end
+    end
+
+    def hidden_ships_grid #return 2D array representing the grid where every :S is replace with an :N
+        hidden_grid = @grid.dup
+        @grid.map(&:object_id)
+        hidden_grid.map(&:object_id)
+        (0...hidden_grid.length).each do | row |
+            (0...hidden_grid[0].length).each do | col |
+                debugger
+                hidden_grid[row][col] = :N if hidden_grid[row][col] == :S
+            end
+        end
+        debugger
+        hidden_grid
+    end
+
+
+
 end
+
+
+
+
+
+
+
