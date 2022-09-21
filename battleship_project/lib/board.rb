f51f723a2d914de2a252c029c509269cb2fc9852
korycfitz <korycfitz@gmail.com>
2022-09-21 09:48:54 -0400

    passed all rspecs for part 1

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -5,4 +5,27 @@ class Board
         @size  = (n * n) #total size of grid
     end
 
+    def [](position) #accepts position on board (array)
+        row = position[0]
+        column = position[1]
+        @grid[row][column] #returns element of grid at position (:Sym)
+    end
+
+    def []=(position, value)
+        row = position[0]
+        column = position[1]
+        @grid[row][column] = value
+    end
+
+    def num_ships
+        counter = 0
+        (0...@grid.length).each do | idx1 |
+            (0...@grid[0].length).each do | idx2 |
+                counter += 1 if @grid[idx1][idx2] == :S
+            end
+        end
+        counter         
+    end
+
+
 end
